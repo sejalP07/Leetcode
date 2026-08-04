b57fbb35473e96b51ba6c1a671d91c4185c3c1bd
Sejal P <sejalmesta007@gmail.com>
2026-08-04 23:23:48 +0530

    Time: 7 ms (71.68%), Space: 19.3 MB (36.55%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        result = "1"
+
+        for _ in range(n - 1):
+            current = []
+            count = 1
+
+            for i in range(1, len(result)):
+                if result[i] == result[i - 1]:
+                    count += 1
+                else:
+                    current.append(str(count))
+                    current.append(result[i - 1])
+                    count = 1
+
+            # Add the last group
+            current.append(str(count))
+            current.append(result[-1])
+
+            result = "".join(current)
+
+        return result
